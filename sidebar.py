3b4ffe6c99bec4dd3af834cf94b45bfbdb58aaf3
gabyang <e0725795@u.nus.edu>
2025-02-16 05:19:45 +0800

    refactor more code

--- sidebar.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-with st.sidebar:
-    st.page_link("https://www.apple.com", label="Home", icon="🏠")
-    st.page_link("https://www.apple.com", label="Opportunities", icon="🏠")
-    st.page_link("https://www.apple.com", label="Home", icon="🏠")
