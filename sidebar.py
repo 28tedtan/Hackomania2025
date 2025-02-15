8865eef34280cfd817531251ac4958fdca68ff98
28tedtan <28tedtan@gmail.com>
2025-02-15 18:33:32 +0800

    sideb

---
+++ sidebar.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+import streamlit as st
+import time
+
+with st.sidebar:
+    st.page_link("https://www.apple.com", label="Home", icon="🏠")
+    st.page_link("https://www.apple.com", label="Opportunities", icon="🏠")
+    st.page_link("https://www.apple.com", label="Home", icon="🏠")
